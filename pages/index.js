a1f90ddad6eced65fcc893ccdbdec40173b40124
shuting72 <ryanliu90114@gmail.com>
2025-05-10 00:22:37 +0800

    Flatten project structure

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <a href="/scoreboard" className="text-blue-500 underline">
+        前往 16 關成績頁
+      </a>
+    </div>
+  );
+}
