fe295542004cd97c1dd03cf2bba1d4dbf8c179e0
shuting72 <ryanliu90114@gmail.com>
2025-05-24 14:57:13 +0800

    調整總表與排行畫面樣式，4x4 正方格，放大光暈與字體

--- pages/display.js
+++ pages/display.js
@@ -42,7 +42,7 @@ export default function DisplayPage() {
       {pageIndex === 0 ? (
         <div className={styles.grid}>
           {dummyRankings.map((game, i) => (
-            <div className={`${styles.card} ${styles['color' + (i % 10)]}`} key={i}>
+            <div className={styles.cell} key={i}>
               <div className={styles.title}>{game.icon} {game.name}</div>
               <div className={styles.score}>成績：--</div>
               <div className={styles.champion}>👑 --</div>
